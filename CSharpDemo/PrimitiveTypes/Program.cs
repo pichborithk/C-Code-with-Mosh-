835fafbc35a70a1b3b4e346fe68fca851b3bd32d
Pichborith Kong <pichborith@gmail.com>
2024-03-15 14:25:40 -0700

    update primitive data

--- CSharpDemo/PrimitiveTypes/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("{0}, {1}", byte.MinValue, byte.MaxValue);
-Console.WriteLine("{0}, {1}", short.MinValue, short.MaxValue);
-Console.WriteLine("{0}, {1}", int.MinValue, int.MaxValue);
-Console.WriteLine("{0}, {1}", long.MinValue, long.MaxValue);
-Console.WriteLine("{0}, {1}", float.MinValue, float.MaxValue);
-Console.WriteLine("{0}, {1}", double.MinValue, double.MaxValue);
-Console.WriteLine("{0}, {1}", decimal.MinValue, decimal.MaxValue);
---
+++ PrimitiveData/Program.cs
@@ -0,0 +1,27 @@
+﻿byte byteMin = byte.MinValue;
+byte byteMax = byte.MaxValue;
+Console.WriteLine("Byte value range: {0} to {1}", byteMin, byteMax);
+
+short shortMin = short.MinValue;
+short shortMax = short.MaxValue;
+Console.WriteLine("Short value range: {0} to {1}", shortMin, shortMax);
+
+int integerMin = int.MinValue;
+int integerMax = int.MaxValue;
+Console.WriteLine("Integer value range: {0} to {1}", integerMin, integerMax);
+
+long longMin = long.MinValue;
+long longMax = long.MaxValue;
+Console.WriteLine("Long value range: {0} to {1}", longMin, longMax);
+
+float floatMin = float.MinValue;
+float floatMax = float.MaxValue;
+Console.WriteLine("Float value range: {0} to {1}", floatMin, floatMax);
+
+double doubleMin = double.MinValue;
+double doubleMax = double.MaxValue;
+Console.WriteLine("Double value range: {0} to {1}", doubleMin, doubleMax);
+
+decimal decimalMin = decimal.MinValue;
+decimal decimalMax = decimal.MaxValue;
+Console.WriteLine("Decimal value range: {0} to {1}", decimalMin, decimalMax);
