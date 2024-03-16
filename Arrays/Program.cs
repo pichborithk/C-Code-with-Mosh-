bea1e3a148d62378d32f5823a2265dfb15a6f09d
Pichborith Kong <pichborith@gmail.com>
2024-03-16 16:58:19 -0700

    update array and add loops

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,7 +1,35 @@
 ﻿/*
     int[] numbers = new int[5] {1, 2, 3, 4, 5};
+    var numbers = new[] {1, 2, 3, 4, 5};
 */
 
 int[] numbers = [1, 2, 3, 4, 5];
 
 Console.WriteLine(string.Join(", ", numbers));
+
+Console.WriteLine("Index of 4: " + Array.IndexOf(numbers, 4));
+
+Array.Clear(numbers, 0, 2);
+
+Console.WriteLine("After Clear from index 0 to 2: " + string.Join(", ", numbers));
+
+Array.Reverse(numbers);
+
+Console.WriteLine("After Reverse: " + string.Join(", ", numbers));
+
+var matrix = new int[3, 4]
+{
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+    {1, 2, 3, 4},
+};
+
+Console.WriteLine($"Length of matrix array is : {matrix.Length}");
+
+foreach (var n in matrix)
+{
+    Console.Write(n + " ");
+}
+
+Console.WriteLine();
+
---
+++ Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numbers = [1, 2, 3, 4, 5];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
+
+foreach (var n in numbers)
+{
+    Console.WriteLine(n);
+}
+
+int j = 0;
+while (j < numbers.Length)
+{
+    Console.WriteLine(numbers[j]);
+    j++;
+}
+
+int k = 0;
+do
+{
+    Console.WriteLine(numbers[k]);
+    k++;
+} while (k < numbers.Length);
