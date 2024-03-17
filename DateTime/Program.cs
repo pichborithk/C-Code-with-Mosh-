27c8b30e7bdaa970e92205642373b229d54d60b8
Pichborith Kong <pichborith@gmail.com>
2024-03-16 23:34:15 -0700

    add date time

---
+++ DateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿var dataTime = new DateTime(2024, 1, 1);
+
+Console.WriteLine(dataTime.ToString());
+
+var today = DateTime.Today;
+
+Console.WriteLine("Current Date: " + today.Date);
+
+var now = DateTime.Now;
+
+Console.WriteLine("Current Hour: " + now.Hour);
+
+/*
+    Console.WriteLine(now.ToLongDateString());
+    Console.WriteLine(now.ToShortDateString());
+    Console.WriteLine(now.ToLongTimeString());
+    Console.WriteLine(now.ToShortTimeString());
+    Console.WriteLine(now.ToString("yyyy-MM-dd"));
+*/
+
+var tomorrow = today.AddDays(1);
+
+Console.WriteLine("Tomorrow is: " + tomorrow.ToString());
+
+var yesterday = today.AddDays(-1);
+
+Console.WriteLine("Yesterday is: " + yesterday.ToString());
