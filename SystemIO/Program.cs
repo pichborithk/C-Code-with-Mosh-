f6ebc7649472e416aff9419f42668b4a1cfb378c
Pichborith Kong <pichborith@gmail.com>
2024-03-17 12:34:52 -0700

    add systemIO

---
+++ SystemIO/Program.cs
@@ -0,0 +1,36 @@
+﻿var path = @"c:\Users\Pichborith Kong\Pictures\";
+
+if (!Directory.Exists(path + "TestSystemIO"))
+{
+    Directory.CreateDirectory(path + "TestSystemIO");
+}
+
+
+if (File.Exists(path + @"\TestSystemIO\Wyven.jpg"))
+{
+    File.Delete(path + @"\TestSystemIO\Wyven.jpg");
+}
+File.Copy(path + "Wyven.jpg", path + @"\TestSystemIO\Wyven.jpg", true);
+
+/*
+    var fileInfo = new FileInfo(path + "Wyven.jpg");
+    if (fileInfo.Exists)
+    {
+        fileInfo.CopyTo(path + @"\TestSystemIO\Wyven.jpg", true);
+    }
+*/
+var files = Directory.GetFiles(path + "TestSystemIO", "*.*", SearchOption.AllDirectories);
+
+foreach (var file in files)
+{
+    Console.WriteLine("Extension: " + Path.GetExtension(file));
+    Console.WriteLine("File Name: " + Path.GetFileName(file));
+    Console.WriteLine("File Name without Extension: " + Path.GetFileNameWithoutExtension(file));
+    Console.WriteLine("Directory Name: " + Path.GetDirectoryName(file));
+    
+}
+/*
+    var directories = Directory.GetDirectories(path);
+
+    foreach (var d in directories) Console.WriteLine(d);
+*/
