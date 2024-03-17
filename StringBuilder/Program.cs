4132df1ac5558a1c5c9350717f57762f8576d6c3
Pichborith Kong <pichborith@gmail.com>
2024-03-16 23:50:15 -0700

    add String Builder

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+var builder = new StringBuilder();
+builder.Append('-', 10).AppendLine();
+
+builder.Append("Header");
+builder.AppendLine();
+builder.Append('-', 10);
+Console.WriteLine(builder);
+builder.Replace('-', '+');
+
+Console.WriteLine(builder);
+
+//builder.Remove(0, 10);
+builder.Insert(10, new string('-', 10));
+
+Console.WriteLine(builder); 
