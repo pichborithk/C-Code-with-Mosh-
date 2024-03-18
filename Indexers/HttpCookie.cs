21a9054e4b8ffe0cdcbcff356d7f570a51441bf8
Pichborith Kong <pichborith@gmail.com>
2024-03-17 22:25:19 -0700

    update indexers

--- Indexers/HttpCookie.cs
+++ Indexers/HttpCookie.cs
@@ -5,17 +5,17 @@
         // private Dictionary<string, string> _dictionary = new Dictionary<string, string>();
         // private Dictionary<string, string> _dictionary = new();
         private readonly Dictionary<string, string> _dictionary = [];
+        /*
+                public string GetItem(string key)
+                {
+                    return _dictionary[key];
+                }
 
-        public string GetItem(string key)
-        {
-            return _dictionary[key];
-        }
-
-        public void SetItem(string key, string value)
-        {
-            _dictionary[key] = value;
-        }
-
+                public void SetItem(string key, string value)
+                {
+                    _dictionary[key] = value;
+                }
+        */
         public string this[string key]
         {
             get { return _dictionary[key]; }
--- Indexers/Program.cs
+++ Indexers/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Indexers;
+
+var cookie = new HttpCookie();
+cookie["name"] = "John";
+Console.WriteLine(cookie["name"]);
+
