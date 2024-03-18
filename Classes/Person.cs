a5023a61541440ea2fa989130aa725568c0db037
Pichborith Kong <pichborith@gmail.com>
2024-03-17 22:11:35 -0700

    update classes

--- Classes/Person.cs
+++ Classes/Person.cs
@@ -5,10 +5,41 @@
         public string FirstName;
         public string LastName;
 
+        /*
+                private DateTime _birthdate;
+                public DateTime BirthDate
+                {
+                    get { return _birthdate; }
+                    set { _birthdate = value; }
+                }
+        */
+        /*
+                private DateTime _birthdate;
+                public DateTime BirthDate
+                {
+                    get { return _birthdate; }
+                    set => _birthdate = value;
+                }
+        */
+        public DateTime BirthDate { get; private set; }
+
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Now - BirthDate;
+                return timeSpan.Days / 365;
+            }
+        }
+
+        public Person(DateTime birthdate)
+        {
+            BirthDate = birthdate;
+        }
+
         public void Introduce()
         {
             Console.WriteLine("My name is " + FirstName + " " + LastName);
         }
-
     }
 }
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,10 +6,11 @@ john.firstName = "John";
 john.lastName = "Smith";
 */
 
-Person john = new()
+Person john = new(new DateTime(1989, 11, 26))
 {
     FirstName = "John",
     LastName = "Smith"
 };
 
 john.Introduce();
+Console.WriteLine("John age is " + john.Age);
