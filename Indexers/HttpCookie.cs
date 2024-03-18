cc84cbc03633bab6b9a673afe1394f72af6cfd67
Pichborith Kong <pichborith@gmail.com>
2024-03-17 22:23:44 -0700

    add indexers

---
+++ Indexers/HttpCookie.cs
@@ -0,0 +1,25 @@
+﻿namespace Indexers
+{
+    public class HttpCookie
+    {
+        // private Dictionary<string, string> _dictionary = new Dictionary<string, string>();
+        // private Dictionary<string, string> _dictionary = new();
+        private readonly Dictionary<string, string> _dictionary = [];
+
+        public string GetItem(string key)
+        {
+            return _dictionary[key];
+        }
+
+        public void SetItem(string key, string value)
+        {
+            _dictionary[key] = value;
+        }
+
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+}
