c97ccbbecbbaab1c1513974d6fc606061b2cb043
Pichborith Kong <pichborith@gmail.com>
2024-03-17 22:10:56 -0700

    add access modifier

---
+++ AccessModifier/Program.cs
@@ -0,0 +1,14 @@
+﻿string cheatsheet = @"
++--------------------+--------------------------------------------------------------+
+| Modifier           | Class |   Derived Class   | Assembly | Derived Class | World |
+|                    |       | (within assembly) |          |    (World)    |       |
++--------------------+--------------------------------------------------------------+
+| public             |   Y   |         Y         |     Y    |        Y      |   Y   |
+| protected internal |   Y   |         Y         |     Y    |        Y      |   N   |
+| protected          |   Y   |         Y         |     N    |        Y      |   N   |
+| internal           |   Y   |         Y         |     Y    |        N      |   N   |
+| private protected  |   Y   |         Y         |     N    |        N      |   N   |
+| private            |   Y   |         N         |     N    |        N      |   N   |
++-----------------------------------------------------------------------------------+";
+
+Console.WriteLine(cheatsheet);
