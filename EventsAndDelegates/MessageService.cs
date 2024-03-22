f2bd42dc62bfe476ee3f0f7b9ba5cdcc726a0c21
Pichborith Kong <pichborith@gmail.com>
2024-03-21 21:52:16 -0700

    add events and delegates

---
+++ EventsAndDelegates/MessageService.cs
@@ -0,0 +1,7 @@
+﻿public class MessageService
+{
+    public void OnVideoEncoded(object source, VideoEventArgs args)
+    {
+        Console.WriteLine("MessageService: sending a text message..." + args.Video.Title);
+    }
+}
