a8c11f6db42d1205588cbabdcc81b8ce4d9d74bc
Pichborith Kong <pichborith@gmail.com>
2024-03-22 15:09:43 -0700

    add Nullable Types

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿// Nullable<DateTime> date = null;
+DateTime? date = null;
+
+Console.WriteLine("GetValueOrDefault: " + date.GetValueOrDefault());
+Console.WriteLine("HasValue: " + date.HasValue);
+/* 
+DateTime date2;
+
+if (date != null)
+{
+    date2 = date.GetValueOrDefault();
+}
+else
+{
+    date2 = DateTime.Today;
+}
+ */
+
+// DateTime date2 = date != null ? date.GetValueOrDefault() : DateTime.Today;
+DateTime date2 = date ?? DateTime.Today;
+
+Console.WriteLine(date2);
