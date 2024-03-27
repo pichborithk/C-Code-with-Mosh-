902b4a99f2d9f4769cec830222b88dd35c5a1964
Pichborith Kong <pichborith@gmail.com>
2024-03-26 17:18:38 -0700

    update loops demo

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,5 @@
-﻿/*
+﻿// Single-Dimensional Array
+/*
     int[] numbers = new int[5] {1, 2, 3, 4, 5};
     var numbers = new[] {1, 2, 3, 4, 5};
 */
@@ -17,6 +18,9 @@ Array.Reverse(numbers);
 
 Console.WriteLine("After Reverse: " + string.Join(", ", numbers));
 
+// Multi-Dimensional Array
+
+// int[,] matrix = new int[3, 4];
 var matrix = new int[3, 4]
 {
     {1, 2, 3, 4},
@@ -31,5 +35,26 @@ foreach (var n in matrix)
     Console.Write(n + " ");
 }
 
-Console.WriteLine();
+Console.WriteLine(matrix[0, 0]);
 
+// Jagged Array (Array of Arrays)
+int[][] jaggedArray = new int[3][]; // Declare a jagged array of size 3
+
+// Initialize each sub-array with different sizes
+jaggedArray[0] = new int[2] { 1, 2 };
+jaggedArray[1] = new int[3] { 3, 4, 5 };
+jaggedArray[2] = new int[4] { 6, 7, 8, 9 };
+
+// Access and print values from the jagged array using nested for loops
+Console.WriteLine("\nJagged Array:");
+for (int i = 0; i < jaggedArray.Length; i++)
+{
+    for (int j = 0; j < jaggedArray[i].Length; j++)
+    {
+        Console.Write(jaggedArray[i][j] + " ");
+    }
+    Console.WriteLine();
+}
+
+// Pause the program to view the output
+Console.ReadLine();
