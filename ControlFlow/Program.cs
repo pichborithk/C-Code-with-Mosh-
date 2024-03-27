69aa4b409ada9cca6a426631da723fe6202fb8d3
Pichborith Kong <pichborith@gmail.com>
2024-03-26 17:14:31 -0700

    add control flow demo

---
+++ ControlFlow/Program.cs
@@ -0,0 +1,28 @@
+﻿// If-Else Statement
+int number = 10;
+if (number > 5)
+{
+    Console.WriteLine("Number is greater than 5");
+}
+else
+{
+    Console.WriteLine("Number is not greater than 5");
+}
+
+// Switch Statement
+char grade = 'B';
+switch (grade)
+{
+    case 'A':
+        Console.WriteLine("Excellent!");
+        break;
+    case 'B':
+        Console.WriteLine("Good!");
+        break;
+    case 'C':
+        Console.WriteLine("Fair!");
+        break;
+    default:
+        Console.WriteLine("Needs improvement!");
+        break;
+}
