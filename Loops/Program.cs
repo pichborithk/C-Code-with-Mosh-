e22afe9cfc63ba2461fd5ac019b55b5ede6b0518
Pichborith Kong <pichborith@gmail.com>
2024-03-26 17:13:30 -0700

    update loop demo

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,25 +1,46 @@
 ﻿int[] numbers = [1, 2, 3, 4, 5];
 
-for (int i = 0; i < numbers.Length; i++)
-{
-    Console.WriteLine(numbers[i]);
-}
-
+// foreach Loop
 foreach (var n in numbers)
 {
     Console.WriteLine(n);
 }
 
-int j = 0;
-while (j < numbers.Length)
+// While Loop
+int i = 0;
+while (i < 5)
 {
-    Console.WriteLine(numbers[j]);
-    j++;
+    Console.WriteLine("While loop iteration: " + i);
+    i++;
 }
 
+// For Loop
+for (int j = 0; j < 5; j++)
+{
+    Console.WriteLine("For loop iteration: " + j);
+}
+
+// Do-While Loop
 int k = 0;
 do
 {
-    Console.WriteLine(numbers[k]);
+    Console.WriteLine("Do-While loop iteration: " + k);
     k++;
-} while (k < numbers.Length);
+} while (k < 5);
+
+// Break and Continue Statements
+for (int l = 0; l < 10; l++)
+{
+    if (l == 5)
+    {
+        break; // Exit the loop when l equals 5
+    }
+    if (l % 2 == 0)
+    {
+        continue; // Skip the current iteration if l is even
+    }
+    Console.WriteLine("Value of l: " + l);
+}
+
+// Pause the program to view the output
+Console.ReadLine();
